9e9ba3ea3796f9facd3246facf474bd7e95046c9
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-09-23 20:42:02 -0500

    component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //template: '<h1>fucker {{ hacker + title }}</h1>'
 })
 export class AppComponent {
   title = 'app';
+  hacker = 'helo world';
 }
+
+
