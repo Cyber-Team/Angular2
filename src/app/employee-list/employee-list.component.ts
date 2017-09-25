703a69bda6f3df176be59ae1d1b1878b9bd6a6a7
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-09-25 01:29:15 -0500

    ngFor Track by added

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -6,14 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
+  employe: any[];
 
-  employe: any[] = [
+
+  constructor() {
+    this.employe = [
     { code: 'em101', name: 'Tom', gender:'Male'},
-   { code: 'em102', name: 'Alex', gender:'Male'},
+    { code: 'em102', name: 'Alex', gender:'Male'},
     { code: 'em103', name: 'Thom', gender:'female'}
   ];
+  }
+  getEmploye() : void{
+    this.employe = [
+      { code: 'em101', name: 'Tom', gender:'Male'},
+      { code: 'em102', name: 'Alex', gender:'Male'},
+      { code: 'em103', name: 'Thom', gender:'female'},
+      { code: 'em104', name: 'hom', gender:'male'}
 
-  constructor() { }
+    ];
+  }
+  trackByEmpCode(index:number, employe: any): string{
+    return employe.code;
+  }
 
   ngOnInit() {
   }
