e787da1792da7e17751e1ce23c1dce68a05caf6d
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-09-27 00:35:42 -0500

    interface added

---
+++ src/app/employe/employee.ts
@@ -0,0 +1,5 @@
+export interface IEmployee {
+  code: string;
+  name: string;
+  gender: string;
+}
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IEmployee } from './employee';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-  employe: any[];
+  employe: IEmployee[];
   //selectedEmployeCount: string = 'All';
   constructor() {
     this.employe = [
